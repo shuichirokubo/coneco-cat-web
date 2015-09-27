a263dd2ee0fa491698c4d551fa04b6e8e6d12678
Yuya Kakui <mail@kakuiyuya.com>
2015-09-27 16:35:39 +0900

    up

--- local_run.sh
+++ local_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export HUBOT_TWITTER_KEY="fCdlZrXTaQFuBcx3SAtq3alJk"
-export HUBOT_TWITTER_SECRET="k3aBK3DZMeM0pl0tYk5sKnxniu8WfDWxgOUpPg1JwHsoxS0mF6"
-export HUBOT_TWITTER_TOKEN="3546734059-qhSWT3CKaeKty6lZJNEYuVCZpUYNw3sS7LvvZxZ"
-export HUBOT_TWITTER_TOKEN_SECRET="BZJLW3xx9zzDEvYIgmdfPqNpXnwyLIeZukyKiQ4LZw4GQ"
+export HUBOT_TWITTER_KEY="3gGBv8Aqtzn9VlkdFlp0ldrtW"
+export HUBOT_TWITTER_SECRET="zcRFVsAu4D1P1DrHZgVN9SFx7XqbbGAI06nR5IlDyUnCw4fZx2"
+export HUBOT_TWITTER_TOKEN="3546734059-GwCE8snuei0FSd0MpOFeWei32mt0MD9GNiMWhdN"
+export HUBOT_TWITTER_TOKEN_SECRET="nYfwEizZ8ZsCyScF5rzOPPKtdQN88OvetidsoRY5UOD0t"
 
 forever start -c coffee node_modules/.bin/hubot -a twitter-userstream -n coneco_cat
