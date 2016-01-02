1523f42c92bcb514fc1a0582a1f55e16c19f9492
Yuya Kakui <mail@kakuiyuya.com>
2016-01-03 00:32:51 +0900

    add facebook login

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
   get 'welcome/index'
 
   get 'welcome/show'
