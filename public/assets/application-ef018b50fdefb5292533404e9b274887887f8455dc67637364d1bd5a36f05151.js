d2bbb63156bd89908b0b19f52a8ff0a5b52497b3
Yuya Kakui <mail@kakuiyuya.com>
2016-01-11 17:32:24 +0900

    up

--- public/assets/application-ef018b50fdefb5292533404e9b274887887f8455dc67637364d1bd5a36f05151.js
+++ public/assets/application-ef018b50fdefb5292533404e9b274887887f8455dc67637364d1bd5a36f05151.js
@@ -13605,8 +13605,9 @@ if("undefined"==typeof jQuery)throw new Error("Bootstrap requires jQuery");+func
 
   $(window).load(function(){
     $("#navigation").sticky({ topSpacing: 0 });
-    set_rakuten_pics();
+    set_ranking_pics();
     set_instagram_pics();
+    set_rakuten_pics();
     set_flickr_pics();
   });
 
@@ -14055,6 +14056,48 @@ var set_rakuten_pics = function() {
         autoPlay: 3000,
       });
     });
+};
+var get_ranking_pics = function() {
+//  var rankingUrl = 'http://coneco.cat2.pics/api/v1/cats/';
+//  var deferred = $.ajax({
+//    url     : rankingUrl,
+//    type    : 'GET',
+//    dataType: 'jsonp',
+//  });
+//  return deferred.promise();
+};
+
+var set_ranking_pics = function() {
+//  get_ranking_pics()
+//    .then(function(pics) {
+//      $.each(pics, function(index, pic) {
+//        var item = String();
+//        var form = String();
+//        form += "<form action='/api/v1/favorites/' method='post'>";
+//        form += "<input type='hidden' name='cat_id' value='" + pic.id + "'>";
+//        form += "<button type='submit' class='btn btn-lg btn-primary'>";
+//        form += "ふぁぼる";
+//        form += "</button>";
+//        form += "</form>";
+//        if (index == 0) {
+//          item += "<div class='container'>";
+//            item += "<div class='row'>";
+//              item += "<div class='col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3'>";
+//                item += "<div class='text-center'>";
+//                  item += "<p class='text-ranking'><B>ふぁぼランキング1位</B></p>";
+//                  item += "<img src='" + pic.image_url + "' class='img-responsive img-round' alt='img'></a>";
+//                  item += form;
+//                item += "</div>";
+//              item += "</div>";
+//            item += "</div>";
+//          item += "</div>";
+//          $('#ranking').append(item);
+//        }
+//        if (index == 0) {
+//          return false;
+//        }
+//      })
+//    })
 };
 /*!
  * Stellar.js v0.6.2
