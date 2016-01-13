5926ea00e6cc6b878f9c7572e25c0a4aea5b4bd2
Yuya Kakui <mail@kakuiyuya.com>
2016-01-13 23:42:11 +0900

    up

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -3,12 +3,12 @@ class FavoriteController < ApplicationController
   def index
     authenticate_user
     @fav_cats = []
-    Favorite.where(user_id: @current_user.id).page(1).each do |favorite|
-      fav_cat = InstagramCat.without_soft_destroyed.where(id: favorite.cat_id).first
-      @fav_cats.push(fav_cat)
-    end
     if user_signed_in?
       @profile = @current_user.image
+      Favorite.where(user_id: @current_user.id).page(1).each do |favorite|
+        fav_cat = InstagramCat.without_soft_destroyed.where(id: favorite.cat_id).first
+        @fav_cats.push(fav_cat)
+      end
     end
   end
 
